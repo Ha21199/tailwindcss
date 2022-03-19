81025749c366bf963f3ee73a8df80fa66a3eb4fa
Ha21199 <92786347+Ha21199@users.noreply.github.com>
2022-03-19 11:03:25 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily:{
+      sans: "'PT-sans',sans-serif",
+    },
+    extend: {
+      colors:{
+        blue: '#0366bd',
+      }
+    },
+  },
+  plugins: [],
+}
